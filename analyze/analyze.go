a751f346d031c183d16a12feb3e5019850e55c20
Mukul <55563106+agarmu@users.noreply.github.com>
2020-05-31 17:15:49 -0500

    Debugging

--- analyze/analyze.go
+++ analyze/analyze.go
@@ -5,16 +5,15 @@ import (
 )
 
 //CowsBulls returns cows and bulls for a specific guess
-func CowsBulls(target, guess string) (int, int) {
+func CowsBulls(target, guess []string) (int, int) {
 	cows, bulls := 0, 0
-	t, g := SplitStr(target), SplitStr(guess)
 
-	if len(t) != len(g) {
-		log.Panicf("Target Length (%d) not equal to guess length (%d)", len(t), len(g))
+	if len(target) != len(guess) {
+		log.Panicf("Target Length (%d) not equal to guess length (%d)", len(target), len(guess))
 	}
-	for i := 0; i < len(t); i++ {
-		for j := 0; j < len(g); j++ {
-			if t[i] == g[j] {
+	for i := 0; i < len(target)-2; i++ {
+		for j := 0; j < len(guess)-2; j++ {
+			if target[i] == guess[j] {
 				if i == j {
 					bulls++
 				} else {
@@ -24,13 +23,4 @@ func CowsBulls(target, guess string) (int, int) {
 		}
 	}
 	return cows, bulls
-}
-
-//SplitStr splits strings
-func SplitStr(str string) []string {
-	var arr []string
-	for _, r := range str {
-		arr = append(arr, string(r))
-	}
-	return arr
 }
